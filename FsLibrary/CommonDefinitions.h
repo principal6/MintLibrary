4b1228b7f2cf3bd9ceba8e712ae6a10ef8a0c46b
Jangwon Jesus Kim <jesuskim666@gmail.com>
2020-08-22 16:11:45 +0900

    코드 정렬 수정

--- FsLibrary/CommonDefinitions.h
+++ FsLibrary/CommonDefinitions.h
@@ -11,32 +11,32 @@
 
 
 #pragma region Integer definitions
-	using				  int8					=   int8_t;
-	using				 int16					=  int16_t;
-	using				 int32					=  int32_t;
-	using				 int64					=  int64_t;
+	using				  int8						=   int8_t;
+	using				 int16						=  int16_t;
+	using				 int32						=  int32_t;
+	using				 int64						=  int64_t;
 
-	using				 uint8					=  uint8_t;
-	using				uint16					= uint16_t;
-	using				uint32					= uint32_t;
-	using				uint64					= uint64_t;
+	using				 uint8						=  uint8_t;
+	using				uint16						= uint16_t;
+	using				uint32						= uint32_t;
+	using				uint64						= uint64_t;
 
 
-	static constexpr	  int8		kInt8Max	= (  int8) 0x7F;
-	static constexpr	  int8		kInt8Min	= (  int8)~0x7F;
-	static constexpr	 int16		kInt16Max	= ( int16) 0x7FFF;
-	static constexpr	 int16		kInt16Min	= ( int16)~0x7FFF;
-	static constexpr	 int32		kInt32Max	= ( int32) 0x7FFFFFFF;
-	static constexpr	 int32		kInt32Min	= ( int32)~0x7FFFFFFF;
-	static constexpr	 int64		kInt64Max	= ( int64) 0x7FFFFFFFFFFFFFFF;
-	static constexpr	 int64		kInt64Min	= ( int64)~0x7FFFFFFFFFFFFFFF;
+	static constexpr	  int8		kInt8Max		= (  int8) 0x7F;
+	static constexpr	  int8		kInt8Min		= (  int8)~0x7F;
+	static constexpr	 int16		kInt16Max		= ( int16) 0x7FFF;
+	static constexpr	 int16		kInt16Min		= ( int16)~0x7FFF;
+	static constexpr	 int32		kInt32Max		= ( int32) 0x7FFFFFFF;
+	static constexpr	 int32		kInt32Min		= ( int32)~0x7FFFFFFF;
+	static constexpr	 int64		kInt64Max		= ( int64) 0x7FFFFFFFFFFFFFFF;
+	static constexpr	 int64		kInt64Min		= ( int64)~0x7FFFFFFFFFFFFFFF;
 
-	static constexpr	 uint8		kUint8Max	= ( uint8) 0xFF;
-	static constexpr	uint16		kUint16Max	= (uint16) 0xFFFF;
-	static constexpr	uint32		kUint32Max	= (uint32) 0xFFFFFFFF;
-	static constexpr	uint64		kUint64Max	= (uint64) 0xFFFFFFFFFFFFFFFF;
+	static constexpr	 uint8		kUint8Max		= ( uint8) 0xFF;
+	static constexpr	uint16		kUint16Max		= (uint16) 0xFFFF;
+	static constexpr	uint32		kUint32Max		= (uint32) 0xFFFFFFFF;
+	static constexpr	uint64		kUint64Max		= (uint64) 0xFFFFFFFFFFFFFFFF;
 
-	static constexpr uint32			kBitsPerByte = 8;
+	static constexpr	uint32		kBitsPerByte	= 8;
 	static_assert(kBitsPerByte == 8, "!!! Bit per Byte 는 반드시 8이어야 합니다 !!!");
 #pragma endregion
 
