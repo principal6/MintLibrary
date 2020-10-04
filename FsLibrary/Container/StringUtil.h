a21897d54cc84da2fc6f8e8be11d877cd4077931
Jangwon Jesus Kim <jesuskim666@gmail.com>
2020-10-05 03:16:05 +0900

    FNV1a 해시 함수 추가

--- FsLibrary/Container/StringUtil.h
+++ FsLibrary/Container/StringUtil.h
@@ -13,6 +13,23 @@ namespace fs
 		{
 			return (nullptr == rawString || '\0' == rawString[0]);
 		}
+
+        uint64 hashRawString64(const char* rawString)
+        {
+            // Hashing algorithm: FNV1a
+
+            static constexpr uint64 kOffset = 0xcbf29ce484222325;
+            static constexpr uint64 kPrime  = 0x00000100000001B3;
+            const size_t rawStringLength = strlen(rawString);
+
+            uint64 hash = kOffset;
+            for (size_t rawStringAt = 0; rawStringAt < rawStringLength; ++rawStringAt)
+            {
+                hash ^= static_cast<uint8>(rawString[rawStringAt]);
+                hash *= kPrime;
+            }
+            return hash;
+        }
 	}
 }
 
