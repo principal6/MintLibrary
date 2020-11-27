54166168e813a83a31d25f7ae701e14ee0250f51
Jangwon Jesus Kim <jesuskim666@gmail.com>
2020-11-27 15:05:49 +0900

    Int2 코드 정리 및 개선

--- FsLibrary/Math/Int2.h
+++ FsLibrary/Math/Int2.h
@@ -32,10 +32,10 @@ namespace fs
 		Int2&			operator/=(const int32 s) noexcept;
 
 	public:
-		const Int2		operator+(const Int2& rhs) const noexcept;
-		const Int2		operator-(const Int2& rhs) const noexcept;
-		const Int2		operator*(const int32 s) const noexcept;
-		const Int2		operator/(const int32 s) const noexcept;
+		Int2			operator+(const Int2& rhs) const noexcept;
+		Int2			operator-(const Int2& rhs) const noexcept;
+		Int2			operator*(const int32 s) const noexcept;
+		Int2			operator/(const int32 s) const noexcept;
 
 	public:
 		int32&			operator[](const uint32 index) noexcept;
@@ -52,8 +52,10 @@ namespace fs
 		void			y(const int32 newY) noexcept;
 		const int32		y() const noexcept;
 
-	private:
-		int32			_i[2];
+	//private:
+	public:
+		int32			_x;
+		int32			_y;
 	};
 }
 
--- FsLibrary/Math/Int2.hpp
+++ FsLibrary/Math/Int2.hpp
@@ -7,117 +7,118 @@
 
 namespace fs
 {
-	inline Int2::Int2()
+	FS_INLINE Int2::Int2()
 		: Int2(0)
 	{
 		__noop;
 	}
 
-	inline Int2::Int2(const int32 s)
+	FS_INLINE Int2::Int2(const int32 s)
 		: Int2(s, s)
 	{
 		__noop;
 	}
 
-	inline Int2::Int2(const int32 x, const int32 y)
-		: _i{ x, y }
+	FS_INLINE Int2::Int2(const int32 x, const int32 y)
+		: _x{ x }
+		, _y{ y }
 	{
 		__noop;
 	}
 
-	inline Int2& Int2::operator+=(const Int2& rhs) noexcept
+	FS_INLINE Int2& Int2::operator+=(const Int2& rhs) noexcept
 	{
-		_i[0] += rhs._i[0];
-		_i[1] += rhs._i[1];
+		_x += rhs._x;
+		_y += rhs._y;
 		return *this;
 	}
 
-	inline Int2& Int2::operator-=(const Int2& rhs) noexcept
+	FS_INLINE Int2& Int2::operator-=(const Int2& rhs) noexcept
 	{
-		_i[0] -= rhs._i[0];
-		_i[1] -= rhs._i[1];
+		_x -= rhs._x;
+		_y -= rhs._y;
 		return *this;
 	}
 
-	inline Int2& Int2::operator*=(const int32 s) noexcept
+	FS_INLINE Int2& Int2::operator*=(const int32 s) noexcept
 	{
-		_i[0] *= s;
-		_i[1] *= s;
+		_x *= s;
+		_y *= s;
 		return *this;
 	}
 
-	inline Int2& Int2::operator/=(const int32 s) noexcept
+	FS_INLINE Int2& Int2::operator/=(const int32 s) noexcept
 	{
-		_i[0] /= s;
-		_i[1] /= s;
+		_x /= s;
+		_y /= s;
 		return *this;
 	}
 
-	inline const Int2 Int2::operator+(const Int2& rhs) const noexcept
+	FS_INLINE Int2 Int2::operator+(const Int2& rhs) const noexcept
 	{
-		return Int2(_i[0] + rhs._i[0], _i[1] + rhs._i[1]);
+		return Int2(_x + rhs._x, _y + rhs._y);
 	}
 
-	inline const Int2 Int2::operator-(const Int2& rhs) const noexcept
+	FS_INLINE Int2 Int2::operator-(const Int2& rhs) const noexcept
 	{
-		return Int2(_i[0] - rhs._i[0], _i[1] - rhs._i[1]);
+		return Int2(_x - rhs._x, _y - rhs._y);
 	}
 
-	inline const Int2 Int2::operator*(const int32 s) const noexcept
+	FS_INLINE Int2 Int2::operator*(const int32 s) const noexcept
 	{
-		return Int2(_i[0] * s, _i[1] * s);
+		return Int2(_x * s, _y * s);
 	}
 
-	inline const Int2 Int2::operator/(const int32 s) const noexcept
+	FS_INLINE Int2 Int2::operator/(const int32 s) const noexcept
 	{
-		return Int2(_i[0] / s, _i[1] / s);
+		return Int2(_x / s, _y / s);
 	}
 
-	inline int32& Int2::operator[](const uint32 index) noexcept
+	FS_INLINE int32& Int2::operator[](const uint32 index) noexcept
 	{
 		FS_ASSERT("김장원", index < 2, "범위를 벗어난 접근입니다.");
-		return _i[index];
+		return (&_x)[index];
 	}
 
-	inline const int32& Int2::operator[](const uint32 index) const noexcept
+	FS_INLINE const int32& Int2::operator[](const uint32 index) const noexcept
 	{
 		FS_ASSERT("김장원", index < 2, "범위를 벗어난 접근입니다.");
-		return _i[index];
+		return (&_x)[index];
 	}
 
-	inline const bool Int2::operator==(const Int2& rhs) const noexcept
+	FS_INLINE const bool Int2::operator==(const Int2& rhs) const noexcept
 	{
-		return (_i[0] == rhs._i[0] && _i[1] == rhs._i[1]);
+		return (_x == rhs._x && _y == rhs._y);
 	}
 
-	inline const bool Int2::operator!=(const Int2& rhs) const noexcept
+	FS_INLINE const bool Int2::operator!=(const Int2& rhs) const noexcept
 	{
 		return !(*this == rhs);
 	}
 
-	inline void Int2::set(const int32 x, const int32 y) noexcept
+	FS_INLINE void Int2::set(const int32 x, const int32 y) noexcept
 	{
-		_i[0] = x;
-		_i[1] = y;
+		_x = x;
+		_y = y;
 	}
 
-	inline void Int2::x(const int32 newX) noexcept
+	FS_INLINE void Int2::x(const int32 newX) noexcept
 	{
-		_i[0] = newX;
+		_x = newX;
 	}
 
-	inline const int32 Int2::x() const noexcept
+	FS_INLINE const int32 Int2::x() const noexcept
 	{
-		return _i[0];
+		return _x;
 	}
 
-	inline void Int2::y(const int32 newY) noexcept
+	FS_INLINE void Int2::y(const int32 newY) noexcept
 	{
-		_i[1] = newY;
+		_y = newY;
 	}
 
-	inline const int32 Int2::y() const noexcept
+	FS_INLINE const int32 Int2::y() const noexcept
 	{
-		return _i[1];
+		return _y;
 	}
 }
