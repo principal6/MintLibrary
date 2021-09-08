57872e70554ec6cd32cabe944a96f7ad88b6b35b
Jangwon Jesus Kim <jesuskim666@gmail.com>
2021-09-09 00:11:27 +0900

    네임스페이스 정리

--- MintRenderingBase/Include/RenderingBaseCommon.h
+++ MintRenderingBase/Include/RenderingBaseCommon.h
@@ -41,9 +41,9 @@ namespace mint
         // Triangle face
         struct Face
         {
-            static constexpr uint8                  kIndexCountPerFace = 3;
+            static constexpr uint8          kIndexCountPerFace = 3;
 
-            mint::Rendering::IndexElementType     _vertexIndexArray[kIndexCountPerFace];
+            Rendering::IndexElementType     _vertexIndexArray[kIndexCountPerFace];
         };
 
         struct Color
@@ -65,12 +65,12 @@ namespace mint
             constexpr               Color(const int32 r, const int32 g, const int32 b, const int32 a) : Color(r / 255.0f, g / 255.0f, b / 255.0f, a / 255.0f) { __noop; }
             constexpr               Color(const float r, const float g, const float b) : Color(r, g, b, 1.0f) { __noop; }
             constexpr               Color(const int32 r, const int32 g, const int32 b) : Color(r, g, b, 255) { __noop; }
-            constexpr               Color(const mint::Float3& rgb) : Color(rgb._x, rgb._y, rgb._z, 1.0f) { __noop; }
-            constexpr explicit      Color(const mint::Float4& float4) : Color(float4._x, float4._y, float4._z, float4._w) { __noop; }
+            constexpr               Color(const Float3& rgb) : Color(rgb._x, rgb._y, rgb._z, 1.0f) { __noop; }
+            constexpr explicit      Color(const Float4& float4) : Color(float4._x, float4._y, float4._z, float4._w) { __noop; }
 
         public:
-                                    operator mint::Float4&() noexcept { return _raw; }
-                                    operator const mint::Float4&() const noexcept { return _raw; }
+                                    operator Float4&() noexcept { return _raw; }
+                                    operator const Float4&() const noexcept { return _raw; }
                                     operator const float*() const noexcept { return _raw._f; }
             Color                   operator*(const Color& rhs) const noexcept { return Color(_raw._x * rhs._raw._x, _raw._y * rhs._raw._y, _raw._z * rhs._raw._z, _raw._w * rhs._raw._w); }
             Color                   operator*(const float s) const noexcept { return Color(_raw * s); }
@@ -102,7 +102,7 @@ namespace mint
 
         public:
             void                    rgb(const Color& rhs) noexcept { _raw._x = rhs._raw._x; _raw._y = rhs._raw._y; _raw._z = rhs._raw._z; }
-            Color                   addedRgb(const float s) const noexcept { return Color(mint::Math::saturate(_raw._x + s), mint::Math::saturate(_raw._y + s), mint::Math::saturate(_raw._z + s), _raw._w); }
+            Color                   addedRgb(const float s) const noexcept { return Color(Math::saturate(_raw._x + s), Math::saturate(_raw._y + s), Math::saturate(_raw._z + s), _raw._w); }
             Color                   addedRgb(const int32 s) const noexcept { return addedRgb(s / 255.0f); }
             Color                   scaledRgb(const float s) const noexcept { return Color(_raw._x * s, _raw._y * s, _raw._z * s, _raw._w); }
             Color                   scaledA(const float s) const noexcept { return Color(_raw._x, _raw._y, _raw._z, _raw._w * s); }
@@ -116,7 +116,7 @@ namespace mint
             constexpr float         toLuma() const noexcept; // Rec. 601
 
         private:
-            mint::Float4            _raw;
+            Float4                  _raw;
         };
 
 
@@ -144,18 +144,18 @@ namespace mint
                                     ~ColorImage() = default;
 
         public:
-            void                    setSize(const mint::Int2& size) noexcept;
-            const mint::Int2&       getSize() const noexcept;
+            void                    setSize(const Int2& size) noexcept;
+            const Int2&             getSize() const noexcept;
 
         public:
             void                    fill(const Color& color) noexcept;
-            void                    fillRect(const mint::Int2& position, const mint::Int2& size, const Color& color) noexcept;
-            void                    fillCircle(const mint::Int2& center, const int32 radius, const Color& color) noexcept;
-            void                    setPixel(const mint::Int2& at, const Color& color) noexcept;
-            const Color&            getPixel(const mint::Int2& at) const noexcept;
-            const Color             getSubPixel(const mint::Float2& at) const noexcept;
-            void                    getAdjacentPixels(const mint::Int2& at, ColorImage::AdjacentPixels& outAdjacentPixels) const noexcept;
-            void                    getCoAdjacentPixels(const mint::Int2& at, ColorImage::CoAdjacentPixels& outCoAdjacentPixels) const noexcept;
+            void                    fillRect(const Int2& position, const Int2& size, const Color& color) noexcept;
+            void                    fillCircle(const Int2& center, const int32 radius, const Color& color) noexcept;
+            void                    setPixel(const Int2& at, const Color& color) noexcept;
+            const Color&            getPixel(const Int2& at) const noexcept;
+            const Color             getSubPixel(const Float2& at) const noexcept;
+            void                    getAdjacentPixels(const Int2& at, ColorImage::AdjacentPixels& outAdjacentPixels) const noexcept;
+            void                    getCoAdjacentPixels(const Int2& at, ColorImage::CoAdjacentPixels& outCoAdjacentPixels) const noexcept;
 
         public:
             const byte*             buildPixelRgbaArray() noexcept;
@@ -165,9 +165,9 @@ namespace mint
             const Color&            getColorFromXy(const uint32 x, const uint32 y) const noexcept;
 
         private:
-            mint::Int2              _size;
-            mint::Vector<Color>     _colorArray;
-            mint::Vector<byte>      _byteArray;
+            Int2                    _size;
+            Vector<Color>           _colorArray;
+            Vector<byte>            _byteArray;
         };
     }
 }
