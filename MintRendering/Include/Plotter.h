975905fbe972c9d8f56ea9637d11723446ec12da
Jangwon Jesus Kim <jesuskim666@gmail.com>
2021-10-20 22:45:11 +0900

    Plotter 코드 정리

--- MintRendering/Include/Plotter.h
+++ MintRendering/Include/Plotter.h
@@ -1,29 +1,30 @@
 #pragma once
 
 
-#ifndef MINT_PLOTTER_H
-#define MINT_PLOTTER_H
+#ifndef MINT_RENDERING_PLOTTER_H
+#define MINT_RENDERING_PLOTTER_H
 
 
 #include <MintCommon/Include/CommonDefinitions.h>
 
 #include <MintContainer/Include/Vector.h>
 
-#include <MintRenderingBase/Include/ShapeFontRendererContext.h>
-
 
 namespace mint
 {
     namespace Rendering
     {
+        class ShapeFontRendererContext;
+
+
         class Plotter
         {
-            static constexpr mint::Float2 kDefaultSize = mint::Float2(400, 300);
-            static constexpr mint::Rendering::Color kAutoColorArray[]{
-                mint::Rendering::Color(    0.0f,    0.5f,    1.0f),
-                mint::Rendering::Color(    1.0f,  0.125f, 0.0625f),
-                mint::Rendering::Color(    1.0f,  0.625f,    0.0f),
-                mint::Rendering::Color(  0.125f,   0.75f,   0.15f),
+            static constexpr Float2 kDefaultSize = Float2(400, 300);
+            static constexpr Color kAutoColorArray[]{
+                Color(    0.0f,    0.5f,    1.0f),
+                Color(    1.0f,  0.125f, 0.0625f),
+                Color(    1.0f,  0.625f,    0.0f),
+                Color(  0.125f,   0.75f,   0.15f),
             };
             static constexpr uint32 kAutoColorCount = _countof(kAutoColorArray);
 
@@ -36,59 +37,59 @@ namespace mint
             };
 
         public:
-                                    Plotter(mint::Rendering::ShapeFontRendererContext& shapeFontRendererContext);
-                                    ~Plotter() = default;
+                                            Plotter(ShapeFontRendererContext& shapeFontRendererContext);
+                                            ~Plotter() = default;
 
         public:
-            void                    scatter(const mint::Vector<float>& xData, const mint::Vector<float>& yData);
+            void                            scatter(const Vector<float>& xData, const Vector<float>& yData);
 
         private:
-            void                    updateFrameValues();
+            void                            updateFrameValues();
 
         public:
-            void                    plotType(const PlotType nextPlotType) noexcept { _nextPlotType = nextPlotType; }
-            void                    xLabel(const std::wstring& label) noexcept { _xLabel = label; }
-            void                    yLabel(const std::wstring& label) noexcept { _yLabel = label; }
-            void                    render() noexcept;
+            MINT_INLINE void                plotType(const PlotType nextPlotType) noexcept { _nextPlotType = nextPlotType; }
+            MINT_INLINE void                xLabel(const std::wstring& label) noexcept { _xLabel = label; }
+            MINT_INLINE void                yLabel(const std::wstring& label) noexcept { _yLabel = label; }
+            void                            render() noexcept;
 
         private:
-            void                    plotScatter(const PlotType plotType, const float x, const float y, const mint::Rendering::Color& color);
-            mint::Float2            computeOrigin() const noexcept;
-            mint::Float2            computeInFramePlotPosition(const float x, const float y) const noexcept;
-            mint::Float2            computePlotPosition(const float x, const float y) const noexcept;
-            void                    drawFrame(const mint::Float4& frameCenterPosition) noexcept;
-            void                    drawLabels(const mint::Float4& frameCenterPosition) noexcept;
+            void                            plotScatter(const PlotType plotType, const float x, const float y, const Color& color);
+            Float2                          computeOrigin() const noexcept;
+            Float2                          computeInFramePlotPosition(const float x, const float y) const noexcept;
+            Float2                          computePlotPosition(const float x, const float y) const noexcept;
+            void                            drawFrame(const Float4& frameCenterPosition) noexcept;
+            void                            drawLabels(const Float4& frameCenterPosition) noexcept;
 
         private:
-            mint::Rendering::ShapeFontRendererContext* const    _shapeFontRendererContext;
+            ShapeFontRendererContext* const _shapeFontRendererContext;
         
         private:
-            mint::Vector<mint::Vector<float>>           _xDataSets;
-            mint::Vector<mint::Vector<float>>           _yDataSets;
-            mint::Vector<mint::Rendering::Color>    _colorArray;
+            Vector<Vector<float>>           _xDataSets;
+            Vector<Vector<float>>           _yDataSets;
+            Vector<Color>                   _colorArray;
         
         private:
-            mint::Vector<PlotType>      _plotTypeArray;
-            PlotType                    _nextPlotType;
+            Vector<PlotType>                _plotTypeArray;
+            PlotType                        _nextPlotType;
         
         private:
-            mint::Float2                _min;
-            mint::Float2                _max;
-            mint::Float2                _range;
-            mint::Float2                _framePadding;
-            mint::Float2                _frameMin;
-            mint::Float2                _frameMax;
-            mint::Float2                _frameRange;
+            Float2                          _min;
+            Float2                          _max;
+            Float2                          _range;
+            Float2                          _framePadding;
+            Float2                          _frameMin;
+            Float2                          _frameMax;
+            Float2                          _frameRange;
 
         private:
-            std::wstring                _xLabel;
-            std::wstring                _yLabel;
-            mint::Float2                _position;
-            mint::Float2                _size;
-            mint::Float2                _frameOffset;
+            std::wstring                    _xLabel;
+            std::wstring                    _yLabel;
+            Float2                          _position;
+            Float2                          _size;
+            Float2                          _frameOffset;
         };
     }
 }
 
 
-#endif // !MINT_PLOTTER_H
+#endif // !MINT_RENDERING_PLOTTER_H
--- MintRendering/Source/Plotter.cpp
+++ MintRendering/Source/Plotter.cpp
@@ -5,25 +5,27 @@
 
 #include <MintContainer/Include/StringUtil.hpp>
 
+#include <MintRenderingBase/Include/ShapeFontRendererContext.h>
+
 
 namespace mint
 {
     namespace Rendering
     {
-        Plotter::Plotter(mint::Rendering::ShapeFontRendererContext& shapeFontRendererContext)
+        Plotter::Plotter(ShapeFontRendererContext& shapeFontRendererContext)
             : _shapeFontRendererContext{ &shapeFontRendererContext }
             , _nextPlotType{ PlotType::Circle }
             , _size{ kDefaultSize }
             , _frameOffset{ 100.0f, 100.0f }
         {
-            _min._x = +mint::Math::kFloatMax;
-            _min._y = +mint::Math::kFloatMax;
+            _min._x = +Math::kFloatMax;
+            _min._y = +Math::kFloatMax;
 
-            _max._x = -mint::Math::kFloatMax;
-            _max._y = -mint::Math::kFloatMax;
+            _max._x = -Math::kFloatMax;
+            _max._y = -Math::kFloatMax;
         }
 
-        void Plotter::scatter(const mint::Vector<float>& xData, const mint::Vector<float>& yData)
+        void Plotter::scatter(const Vector<float>& xData, const Vector<float>& yData)
         {
             _xDataSets.push_back(xData);
             _yDataSets.push_back(yData);
@@ -31,18 +33,18 @@ namespace mint
             const uint32 dataCount = xData.size();
             for (uint32 dataIndex = 0; dataIndex < dataCount; ++dataIndex)
             {
-                _min._x = mint::min(_min._x, xData[dataIndex]);
-                _min._y = mint::min(_min._y, yData[dataIndex]);
+                _min._x = min(_min._x, xData[dataIndex]);
+                _min._y = min(_min._y, yData[dataIndex]);
 
-                _max._x = mint::max(_max._x, xData[dataIndex]);
-                _max._y = mint::max(_max._y, yData[dataIndex]);
+                _max._x = max(_max._x, xData[dataIndex]);
+                _max._y = max(_max._y, yData[dataIndex]);
             }
 
             _range = _max - _min;
             
             updateFrameValues();
 
-            _colorArray.push_back(mint::Rendering::Color::kTransparent);
+            _colorArray.push_back(Color::kTransparent);
             _plotTypeArray.push_back(_nextPlotType);
         }
 
@@ -65,8 +67,8 @@ namespace mint
 
         void Plotter::render() noexcept
         {
-            const mint::Float2 centerPosition = _position +_size * 0.5f;
-            mint::Float4 frameCenterPosition = mint::Float4(centerPosition._x + _frameOffset._x, centerPosition._y + _frameOffset._y, 0.0f, 1.0f);
+            const Float2 centerPosition = _position +_size * 0.5f;
+            Float4 frameCenterPosition = Float4(centerPosition._x + _frameOffset._x, centerPosition._y + _frameOffset._y, 0.0f, 1.0f);
 
             drawFrame(frameCenterPosition);
             drawLabels(frameCenterPosition);
@@ -75,9 +77,9 @@ namespace mint
             const uint32 setCount = _xDataSets.size();
             for (uint32 setIndex = 0; setIndex < setCount; ++setIndex)
             {
-                const bool useAutoColor = (_colorArray[setIndex] == mint::Rendering::Color::kTransparent);
+                const bool useAutoColor = (_colorArray[setIndex] == Color::kTransparent);
                 const PlotType plotType = _plotTypeArray[setIndex];
-                const mint::Rendering::Color& color = (useAutoColor == true) ? kAutoColorArray[autoColorIndex] : _colorArray[setIndex];
+                const Color& color = (useAutoColor == true) ? kAutoColorArray[autoColorIndex] : _colorArray[setIndex];
 
                 const uint32 dataCount = _xDataSets[setIndex].size();
                 for (uint32 dataIndex = 0; dataIndex < dataCount; ++dataIndex)
@@ -101,40 +103,40 @@ namespace mint
             _shapeFontRendererContext->renderAndFlush();
         }
         
-        void Plotter::plotScatter(const PlotType plotType, const float x, const float y, const mint::Rendering::Color& color)
+        void Plotter::plotScatter(const PlotType plotType, const float x, const float y, const Color& color)
         {
-            const mint::Float2 plotPosition2 = computePlotPosition(x, y);
+            const Float2 plotPosition2 = computePlotPosition(x, y);
             
             _shapeFontRendererContext->setColor(color);
-            _shapeFontRendererContext->setPosition(mint::Float4(plotPosition2._x, plotPosition2._y, 0.0f, 1.0f));
+            _shapeFontRendererContext->setPosition(Float4(plotPosition2._x, plotPosition2._y, 0.0f, 1.0f));
 
             switch (plotType)
             {
-            case mint::Rendering::Plotter::PlotType::Circle:
+            case Plotter::PlotType::Circle:
             {
                 const float kRadius = 4.0f;
                 _shapeFontRendererContext->drawCircle(kRadius);
                 break;
             }   
-            case mint::Rendering::Plotter::PlotType::X:
+            case Plotter::PlotType::X:
             {
                 const float kLength = 4.0f;
                 const float kHalfLength = kLength * 0.5f;
                 const float kThickness = 2.0f;
-                _shapeFontRendererContext->drawLine(plotPosition2 - mint::Float2(kHalfLength), plotPosition2 + mint::float2(kLength), kThickness);
-                _shapeFontRendererContext->drawLine(plotPosition2 - mint::Float2(kHalfLength, -kLength), plotPosition2 + mint::float2(kLength, -kHalfLength), kThickness);
+                _shapeFontRendererContext->drawLine(plotPosition2 - Float2(kHalfLength), plotPosition2 + float2(kLength), kThickness);
+                _shapeFontRendererContext->drawLine(plotPosition2 - Float2(kHalfLength, -kLength), plotPosition2 + float2(kLength, -kHalfLength), kThickness);
                 break;
             }
-            case mint::Rendering::Plotter::PlotType::Triangle:
+            case Plotter::PlotType::Triangle:
             {
                 static const float kSqrt3 = sqrt(3.0f);
                 const float kHeight = 8.0f;
                 const float kHalfHeight = kHeight * 0.5f;
                 const float kHalfWidth = kHeight / kSqrt3;
                 _shapeFontRendererContext->drawSolidTriangle(
-                    plotPosition2 + mint::Float2(-kHalfWidth, +kHalfHeight), 
-                    plotPosition2 + mint::float2(0.0f, -kHalfHeight),
-                    plotPosition2 + mint::Float2(+kHalfWidth, +kHalfHeight));
+                    plotPosition2 + Float2(-kHalfWidth, +kHalfHeight), 
+                    plotPosition2 + float2(0.0f, -kHalfHeight),
+                    plotPosition2 + Float2(+kHalfWidth, +kHalfHeight));
                 break;
             }
             default:
@@ -142,46 +144,46 @@ namespace mint
             }
         }
 
-        mint::Float2 Plotter::computeOrigin() const noexcept
+        Float2 Plotter::computeOrigin() const noexcept
         {
-            return _frameOffset + mint::Float2(0.0f, _size._y);
+            return _frameOffset + Float2(0.0f, _size._y);
         }
 
-        mint::Float2 Plotter::computeInFramePlotPosition(const float x, const float y) const noexcept
+        Float2 Plotter::computeInFramePlotPosition(const float x, const float y) const noexcept
         {
             const float xRatio = (x - _frameMin._x) / _frameRange._x;
             const float yRatio = (y - _frameMin._y) / _frameRange._y;
-            return mint::Float2(xRatio * _size._x, yRatio * _size._y);
+            return Float2(xRatio * _size._x, yRatio * _size._y);
         }
 
-        mint::Float2 Plotter::computePlotPosition(const float x, const float y) const noexcept
+        Float2 Plotter::computePlotPosition(const float x, const float y) const noexcept
         {
-            const mint::Float2 inFramePlotPosition = computeInFramePlotPosition(x, y);
-            const mint::Float2 origin = computeOrigin();
-            return mint::Float2(origin._x + inFramePlotPosition._x, origin._y - inFramePlotPosition._y);
+            const Float2 inFramePlotPosition = computeInFramePlotPosition(x, y);
+            const Float2 origin = computeOrigin();
+            return Float2(origin._x + inFramePlotPosition._x, origin._y - inFramePlotPosition._y);
         }
 
-        void Plotter::drawFrame(const mint::Float4& frameCenterPosition) noexcept
+        void Plotter::drawFrame(const Float4& frameCenterPosition) noexcept
         {
-            _shapeFontRendererContext->setBorderColor(mint::Rendering::Color::kBlack);
-            _shapeFontRendererContext->setColor(mint::Rendering::Color::kWhite);
+            _shapeFontRendererContext->setBorderColor(Color::kBlack);
+            _shapeFontRendererContext->setColor(Color::kWhite);
             _shapeFontRendererContext->setPosition(frameCenterPosition);
             _shapeFontRendererContext->drawRectangle(_size, 1.0f, 0.0f);
         }
 
-        void Plotter::drawLabels(const mint::Float4& frameCenterPosition) noexcept
+        void Plotter::drawLabels(const Float4& frameCenterPosition) noexcept
         {
             const float paddingY = 10.0f;
-            mint::Float4 labelPosition = mint::Float4(frameCenterPosition._x, frameCenterPosition._y + _size._y * 0.5f + paddingY, 0.0f, 1.0f);
-            mint::Rendering::FontRenderingOption labelRenderingOption;
-            labelRenderingOption._directionHorz = mint::Rendering::TextRenderDirectionHorz::Centered;
-            _shapeFontRendererContext->setTextColor(mint::Rendering::Color::kBlack);
+            Float4 labelPosition = Float4(frameCenterPosition._x, frameCenterPosition._y + _size._y * 0.5f + paddingY, 0.0f, 1.0f);
+            FontRenderingOption labelRenderingOption;
+            labelRenderingOption._directionHorz = TextRenderDirectionHorz::Centered;
+            _shapeFontRendererContext->setTextColor(Color::kBlack);
             _shapeFontRendererContext->drawDynamicText(_xLabel.c_str(), static_cast<uint32>(_xLabel.length()), labelPosition, labelRenderingOption);
 
             const float paddingX = -2.0f;
             const float textWidth = _shapeFontRendererContext->calculateTextWidth(_yLabel.c_str(), static_cast<uint32>(_yLabel.length()));
-            labelPosition = mint::Float4(frameCenterPosition._x - _size._x * 0.5f - paddingX, frameCenterPosition._y + textWidth * 0.5f, 0.0f, 1.0f);
-            labelRenderingOption._transformMatrix = mint::Float4x4::rotationMatrixZ(-mint::Math::kPiOverTwo);
+            labelPosition = Float4(frameCenterPosition._x - _size._x * 0.5f - paddingX, frameCenterPosition._y + textWidth * 0.5f, 0.0f, 1.0f);
+            labelRenderingOption._transformMatrix = Float4x4::rotationMatrixZ(-Math::kPiOverTwo);
             _shapeFontRendererContext->drawDynamicText(_yLabel.c_str(), static_cast<uint32>(_yLabel.length()), labelPosition, labelRenderingOption);
         }
     }
