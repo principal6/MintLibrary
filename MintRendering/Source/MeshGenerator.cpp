60e526d377491044e61a5381832a66fdb16c57e9
Jangwon Jesus Kim <jesuskim666@gmail.com>
2021-12-14 00:59:31 +0900

    generateHalfOpenOctahedron 코드 개선

--- MintRendering/Source/MeshGenerator.cpp
+++ MintRendering/Source/MeshGenerator.cpp
@@ -410,20 +410,9 @@ namespace mint
                 pushPosition({ ::cos(Math::kTwoPi * 0.5f ) * radiusParam._radius, 0.0f, ::sin(Math::kTwoPi * 0.5f ) * radiusParam._radius }, meshData);
                 pushPosition({ ::cos(Math::kTwoPi * 0.75f) * radiusParam._radius, 0.0f, ::sin(Math::kTwoPi * 0.75f) * radiusParam._radius }, meshData);
             }
-            const int32 positionIndexTopCenter = 0;
 
-            // Upper
-            {
-                const int16 positionIndexBase = 1;
-                const Float2 uvs[3]{ Float2(0.5f, 0.0f), Float2(1.0f, 1.0f), Float2(0.0f, 1.0f) };
-                for (int16 sideIndex = 0; sideIndex < 3; ++sideIndex)
-                {
-                    const int16 positionIndex = positionIndexBase + sideIndex;
-                    pushTri({ positionIndexTopCenter, positionIndex + 1, positionIndex }, meshData, uvs);
-                }
-                const int16 positionIndex = positionIndexBase + 3;
-                pushTri({ positionIndexTopCenter, 1, positionIndex }, meshData, uvs);
-            }
+            const int32 positionIndexTopCenter = 0;
+            _pushUpperUmbrellaTris(positionIndexTopCenter, 1, 4, meshData);
         }
 
         void MeshGenerator::generateGeoSphere(const GeoSphereParam& geoSphereParam, MeshData& meshData) noexcept
