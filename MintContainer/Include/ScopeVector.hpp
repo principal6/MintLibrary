1e17df2c84236718e5af4322657b35be9708f88d
Jangwon Jesus Kim <jesuskim666@gmail.com>
2022-03-09 18:20:40 +0900

    ScopeVector 추가

---
+++ MintContainer/Include/ScopeVector.hpp
@@ -0,0 +1,156 @@
+#pragma once
+
+
+#ifndef _MINT_CONTAINER_SCOPE_VECTOR_HPP_
+#define _MINT_CONTAINER_SCOPE_VECTOR_HPP_
+
+
+#include <MintContainer/Include/ScopeVector.h>
+
+
+namespace mint
+{
+    template<typename T, const uint32 Capacity>
+    inline ScopeVector<T, Capacity>::ScopeVector()
+        : _array{}
+        , _size{ 0 }
+    {
+        __noop;
+    }
+
+    template<typename T, uint32 Capacity>
+    inline ScopeVector<T, Capacity>::~ScopeVector()
+    {
+        __noop;
+    }
+
+    template<typename T, uint32 Capacity>
+    inline T& ScopeVector<T, Capacity>::operator[](const uint32 index)
+    {
+        MINT_ASSERT(index < _size, "범위를 벗어난 접근입니다. [index: %d / size: %d]", index, _size);
+        return _array[index];
+    }
+
+    template<typename T, uint32 Capacity>
+    inline const T& ScopeVector<T, Capacity>::operator[](const uint32 index) const
+    {
+        MINT_ASSERT(index < _size, "범위를 벗어난 접근입니다. [index: %d / size: %d]", index, _size);
+        return _array[index];
+    }
+
+    template<typename T, uint32 Capacity>
+    inline void ScopeVector<T, Capacity>::resize(const uint32 size)
+    {
+        MINT_ASSERT(size <= Capacity, "size 는 Capacity 보다 커질 수 없습니다!", size, Capacity);
+        if (size == _size)
+        {
+            return;
+        }
+
+        _size = size;
+    }
+
+    template<typename T, uint32 Capacity>
+    inline void ScopeVector<T, Capacity>::push_back(const T& entry)
+    {
+        if (full())
+        {
+            MINT_ASSERT(false, "ScopeVector 가 이미 가득 차있습니다!");
+            return;
+        }
+
+        _array[_size] = entry;
+        ++_size;
+    }
+
+    template<typename T, uint32 Capacity>
+    inline void ScopeVector<T, Capacity>::push_back(T&& entry)
+    {
+        if (full())
+        {
+            MINT_ASSERT(false, "ScopeVector 가 이미 가득 차있습니다!");
+            return;
+        }
+
+        _array[_size] = entry;
+        ++_size;
+    }
+
+    template<typename T, uint32 Capacity>
+    inline void ScopeVector<T, Capacity>::pop_back()
+    {
+        if (empty())
+        {
+            MINT_ASSERT(false, "ScopeVector 가 이미 비어 있습니다!");
+            return;
+        }
+
+        --_size;
+    }
+
+    template<typename T, uint32 Capacity>
+    inline void ScopeVector<T, Capacity>::clear()
+    {
+        while (empty() == false)
+        {
+            pop_back();
+        }
+    }
+
+    template<typename T, uint32 Capacity>
+    inline T& ScopeVector<T, Capacity>::front()
+    {
+        if (_size == 0)
+        {
+            MINT_NEVER;
+        }
+        return _array[0];
+    }
+
+    template<typename T, uint32 Capacity>
+    inline const T& ScopeVector<T, Capacity>::front() const
+    {
+        if (_size == 0)
+        {
+            MINT_NEVER;
+        }
+        return _array[0];
+    }
+
+    template<typename T, uint32 Capacity>
+    inline T& ScopeVector<T, Capacity>::back()
+    {
+        if (_size == 0)
+        {
+            MINT_NEVER;
+        }
+        return _array[_size - 1];
+    }
+
+    template<typename T, uint32 Capacity>
+    inline const T& ScopeVector<T, Capacity>::back() const
+    {
+        if (_size == 0)
+        {
+            MINT_NEVER;
+        }
+        return _array[_size - 1];
+    }
+
+    template<typename T, uint32 Capacity>
+    MINT_INLINE T& ScopeVector<T, Capacity>::at(const uint32 index) noexcept
+    {
+        MINT_ASSERT(index < _size, "범위를 벗어난 접근입니다.");
+        return _array[mint::min(index, _size - 1)];
+    }
+
+    template<typename T, uint32 Capacity>
+    MINT_INLINE const T& ScopeVector<T, Capacity>::at(const uint32 index) const noexcept
+    {
+        MINT_ASSERT(index < _size, "범위를 벗어난 접근입니다.");
+        return _array[mint::min(index, _size - 1)];
+    }
+}
+
+
+#endif // !_MINT_CONTAINER_SCOPE_VECTOR_HPP_
