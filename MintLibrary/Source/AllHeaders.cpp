da199d710a6eaebec33f99c81e6145741e7746ac
Jangwon Jesus Kim <jesuskim666@gmail.com>
2022-06-09 21:52:03 +0900

    MintLibrary initialize 에서 자동 경로 선택 방법 개선

--- MintLibrary/Source/AllHeaders.cpp
+++ MintLibrary/Source/AllHeaders.cpp
@@ -10,15 +10,21 @@ namespace mint
 
     void Library::initialize() noexcept
     {
-        if (mint::FileUtil::exists("../MintLibrary/Include/Assets"))
+        // #1(개발용): MintLibrary 솔루션에서 MintLibraryTest 가 시작 프로젝트이고, Working Directory 가 $(ProjectDir) 일 경우
+        if (mint::FileUtil::exists("../Assets"))
         {
-            // Working Directory 가 $(ProjectDir) 일 경우
+            mint::Path::setAssetDirectory("../Assets/");
+            mint::Path::setIncludeAssetDirectory("../Assets/Include/");
+        }
+        // #2(배포용): 신규 솔루션에서 Working Directory 가 $(ProjectDir) 일 경우
+        else if (mint::FileUtil::exists("../MintLibrary/Include/Assets"))
+        {
             mint::Path::setAssetDirectory("../MintLibrary/Include/Assets/");
             mint::Path::setIncludeAssetDirectory("../MintLibrary/Include/Assets/Include/");
         }
+        // #3(Fallback): 기본 세팅
         else
         {
-            // MintLibraryTest 프로젝트 세팅
             mint::Path::setAssetDirectory("Assets/");
             mint::Path::setIncludeAssetDirectory("Assets/Include/");
         }
