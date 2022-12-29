5e796bc5e00865d19475492e1ce600c2e2bf8f13
Jangwon Jesus Kim <jesuskim666@gmail.com>
2022-12-29 16:12:09 +0900

    Create GenerateUpstream.bat

---
+++ GenerateUpstream.bat
@@ -0,0 +1,17 @@
+@echo off
+echo Generate /Upstream directory, containing files for importing MintLibrary
+
+: Project Directories
+robocopy ./ Upstream/MintLibrary stdafx.h
+robocopy MintCommon Upstream/MintLibrary/MintCommon /S
+robocopy MintContainer Upstream/MintLibrary/MintContainer /S
+robocopy MintGame Upstream/MintLibrary/MintGame /S
+robocopy MintLanguage Upstream/MintLibrary/MintLanguage /S
+robocopy MintLibrary Upstream/MintLibrary/MintLibrary /S
+robocopy MintMath Upstream/MintLibrary/MintMath /S
+robocopy MintPlatform Upstream/MintLibrary/MintPlatform /S
+robocopy MintReflection Upstream/MintLibrary/MintReflection /S
+robocopy MintRendering Upstream/MintLibrary/MintRendering /S
+robocopy MintRenderingBase Upstream/MintLibrary/MintRenderingBase /S
+
+pause
