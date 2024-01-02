22efa61c6f9d1c1ff856b998e9aea9ba1a2c0fca
Jangwon Kim <jesuskim666@gmail.com>
2024-01-03 02:32:02 +0900

    Update test.cpp

--- MintLibraryTest/test.cpp
+++ MintLibraryTest/test.cpp
@@ -157,10 +157,12 @@ bool Run2DTestWindow(mint::Window& window, mint::Rendering::GraphicDevice& graph
 			graphicDevice.SetSolidCullFrontRasterizer();
 			graphicDevice.SetScreenSpace2DProjectionMatrix();
 			ShapeRendererContext& shapeRendererContext = graphicDevice.GetShapeRendererContext();
-			StackStringW<100> fpsString;
-			FormatString(fpsString, L"FPS: %d", Profiler::FPSCounter::GetFPS());
-			shapeRendererContext.SetTextColor(Color::kBlack);
-			shapeRendererContext.DrawDynamicText(fpsString.CString(), Float2(10, 10), FontRenderingOption());
+			{
+				StackStringW<100> fpsString;
+				FormatString(fpsString, L"FPS: %d", Profiler::FPSCounter::GetFPS());
+				shapeRendererContext.SetTextColor(Color::kBlack);
+				shapeRendererContext.DrawDynamicText(fpsString.CString(), Float2(10, 10), FontRenderingOption());
+			}
 			shapeRendererContext.Render();
 			shapeRendererContext.Flush();
 
