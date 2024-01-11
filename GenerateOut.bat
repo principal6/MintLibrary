9fe49b4868c808ad85f1bc2c4ae7f8ad7ef50b72
Jangwon Kim <jesuskim666@gmail.com>
2024-01-11 22:27:30 +0900

    Update GenerateOut.bat

--- GenerateOut.bat
+++ GenerateOut.bat
@@ -2,52 +2,55 @@
 echo Generate /Out directory, containing files for importing MintLibrary
 
 : Assets Directory
-robocopy Assets Out/MintLibrary/Assets /S
+robocopy Assets Out/Assets /S
+
+: Natvis Files
+robocopy MintContainer/ Out/Natvis *.natvis /xd Out
+robocopy MintMath/ Out/Natvis *.natvis /xd Out
 
 : Include Directory
-robocopy ./ Out/MintLibrary/Include stdafx.h
-robocopy MintAudio/Include Out/MintLibrary/Include/MintAudio/Include /S
-robocopy MintCommon/Include Out/MintLibrary/Include/MintCommon/Include /S
-robocopy MintContainer/Include Out/MintLibrary/Include/MintContainer/Include /S
-robocopy MintGame/Include Out/MintLibrary/Include/MintGame/Include /S
-robocopy MintGUI/Include Out/MintLibrary/Include/MintGUI/Include /S
-robocopy MintLanguage/Include Out/MintLibrary/Include/MintLanguage/Include /S
-robocopy MintLibrary/Include Out/MintLibrary/Include/MintLibrary/Include /S
-robocopy MintMath/Include Out/MintLibrary/Include/MintMath/Include /S
-robocopy MintPhysics/Include Out/MintLibrary/Include/MintPhysics/Include /S
-robocopy MintPlatform/Include Out/MintLibrary/Include/MintPlatform/Include /S
-robocopy MintReflection/Include Out/MintLibrary/Include/MintReflection/Include /S
-robocopy MintRendering/Include Out/MintLibrary/Include/MintRendering/Include /S
-robocopy MintRenderingBase/Include Out/MintLibrary/Include/MintRenderingBase/Include /S
+robocopy MintAudio/Include Out/Include/MintAudio/Include /S
+robocopy MintCommon/Include Out/Include/MintCommon/Include /S
+robocopy MintContainer/Include Out/Include/MintContainer/Include /S
+robocopy MintGame/Include Out/Include/MintGame/Include /S
+robocopy MintGUI/Include Out/Include/MintGUI/Include /S
+robocopy MintLanguage/Include Out/Include/MintLanguage/Include /S
+robocopy MintLibrary/Include Out/Include/MintLibrary/Include /S
+robocopy MintMath/Include Out/Include/MintMath/Include /S
+robocopy MintPhysics/Include Out/Include/MintPhysics/Include /S
+robocopy MintPlatform/Include Out/Include/MintPlatform/Include /S
+robocopy MintReflection/Include Out/Include/MintReflection/Include /S
+robocopy MintRendering/Include Out/Include/MintRendering/Include /S
+robocopy MintRenderingBase/Include Out/Include/MintRenderingBase/Include /S
 
 : Lib Directory (DEBUG)
-robocopy _output/Debug Out/MintLibrary/Lib/Debug MintAudio.lib
-robocopy _output/Debug Out/MintLibrary/Lib/Debug MintCommon.lib
-robocopy _output/Debug Out/MintLibrary/Lib/Debug MintContainer.lib
-robocopy _output/Debug Out/MintLibrary/Lib/Debug MintGame.lib
-robocopy _output/Debug Out/MintLibrary/Lib/Debug MintGUI.lib
-robocopy _output/Debug Out/MintLibrary/Lib/Debug MintLanguage.lib
-robocopy _output/Debug Out/MintLibrary/Lib/Debug MintLibrary.lib
-robocopy _output/Debug Out/MintLibrary/Lib/Debug MintMath.lib
-robocopy _output/Debug Out/MintLibrary/Lib/Debug MintPhysics.lib
-robocopy _output/Debug Out/MintLibrary/Lib/Debug MintPlatform.lib
-robocopy _output/Debug Out/MintLibrary/Lib/Debug MintReflection.lib
-robocopy _output/Debug Out/MintLibrary/Lib/Debug MintRendering.lib
-robocopy _output/Debug Out/MintLibrary/Lib/Debug MintRenderingBase.lib
+robocopy _output/Debug Out/Lib/Debug MintAudio.lib
+robocopy _output/Debug Out/Lib/Debug MintCommon.lib
+robocopy _output/Debug Out/Lib/Debug MintContainer.lib
+robocopy _output/Debug Out/Lib/Debug MintGame.lib
+robocopy _output/Debug Out/Lib/Debug MintGUI.lib
+robocopy _output/Debug Out/Lib/Debug MintLanguage.lib
+robocopy _output/Debug Out/Lib/Debug MintLibrary.lib
+robocopy _output/Debug Out/Lib/Debug MintMath.lib
+robocopy _output/Debug Out/Lib/Debug MintPhysics.lib
+robocopy _output/Debug Out/Lib/Debug MintPlatform.lib
+robocopy _output/Debug Out/Lib/Debug MintReflection.lib
+robocopy _output/Debug Out/Lib/Debug MintRendering.lib
+robocopy _output/Debug Out/Lib/Debug MintRenderingBase.lib
 
 : Lib Directory (RELEASE)
-robocopy _output/Release Out/MintLibrary/Lib/Release MintAudio.lib
-robocopy _output/Release Out/MintLibrary/Lib/Release MintCommon.lib
-robocopy _output/Release Out/MintLibrary/Lib/Release MintContainer.lib
-robocopy _output/Release Out/MintLibrary/Lib/Release MintGame.lib
-robocopy _output/Release Out/MintLibrary/Lib/Release MintGUI.lib
-robocopy _output/Release Out/MintLibrary/Lib/Release MintLanguage.lib
-robocopy _output/Release Out/MintLibrary/Lib/Release MintLibrary.lib
-robocopy _output/Release Out/MintLibrary/Lib/Release MintMath.lib
-robocopy _output/Release Out/MintLibrary/Lib/Release MintPhysics.lib
-robocopy _output/Release Out/MintLibrary/Lib/Release MintPlatform.lib
-robocopy _output/Release Out/MintLibrary/Lib/Release MintReflection.lib
-robocopy _output/Release Out/MintLibrary/Lib/Release MintRendering.lib
-robocopy _output/Release Out/MintLibrary/Lib/Release MintRenderingBase.lib
+robocopy _output/Release Out/Lib/Release MintAudio.lib
+robocopy _output/Release Out/Lib/Release MintCommon.lib
+robocopy _output/Release Out/Lib/Release MintContainer.lib
+robocopy _output/Release Out/Lib/Release MintGame.lib
+robocopy _output/Release Out/Lib/Release MintGUI.lib
+robocopy _output/Release Out/Lib/Release MintLanguage.lib
+robocopy _output/Release Out/Lib/Release MintLibrary.lib
+robocopy _output/Release Out/Lib/Release MintMath.lib
+robocopy _output/Release Out/Lib/Release MintPhysics.lib
+robocopy _output/Release Out/Lib/Release MintPlatform.lib
+robocopy _output/Release Out/Lib/Release MintReflection.lib
+robocopy _output/Release Out/Lib/Release MintRendering.lib
+robocopy _output/Release Out/Lib/Release MintRenderingBase.lib
 
 pause
