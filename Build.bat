3105de6a9981709117a978e5ccda96fb11923cab
principal6 <jesuskim666@gmail.com>
2025-05-16 02:24:12 +0900

    Update Build.bat

--- Build.bat
+++ Build.bat
@@ -5,7 +5,7 @@ echo.
 echo.
 
 
-set COMMON_COMP_OPTS="/I. /IExternals\freetype_2_10_4\include\ /IExternals\stb_image\ /std:c++17 /EHsc /D_UNICODE /DUNICODE /D_CONSOLE"
+set COMMON_COMP_OPTS="/I. /IExternals\freetype_2_10_4\include\ /IExternals\stb_image\ /std:c++20 /EHsc /D_UNICODE /DUNICODE /D_CONSOLE"
 set COMMON_COMP_OPTS=%COMMON_COMP_OPTS:"=%
 :### SELECT BUILD MODE
 set /a DEBUG_MODE=0
@@ -25,6 +25,7 @@ set MODED_LIBS=%MODED_LIBS:"=%
 
 :### PREPARE DIRECTORIES
 IF NOT EXIST _intermediate mkdir _intermediate
+IF NOT EXIST _intermediate\MintApp mkdir _intermediate\MintApp
 IF NOT EXIST _intermediate\MintAudio mkdir _intermediate\MintAudio
 IF NOT EXIST _intermediate\MintCommon mkdir _intermediate\MintCommon
 IF NOT EXIST _intermediate\MintContainer mkdir _intermediate\MintContainer
@@ -43,7 +44,7 @@ IF NOT EXIST _output mkdir _output
 
 :### COMPILE
 cl MintCommon\Source\CommonDefinitions.cpp /c /Fo_intermediate\MintCommon\ /Fd_output\MintCommon.pdb
-cl MintContainer\Source\Test.cpp /c /Fo_intermediate\MintContainer\ /Fd_output\MintContainer.pdb
+cl MintContainer\Source\_UnityBuild.cpp /c /Fo_intermediate\MintContainer\ /Fd_output\MintContainer.pdb
 cl MintMath\Source\_UnityBuild.cpp /c /Fo_intermediate\MintMath\ /Fd_output\MintMath.pdb
 cl MintPlatform\Source\_UnityBuild.cpp /c /Fo_intermediate\MintPlatform\ /Fd_output\MintPlatform.pdb
 cl MintReflection\Source\_UnityBuild.cpp /c /Fo_intermediate\MintReflection\ /Fd_output\MintReflection.pdb
@@ -53,12 +54,13 @@ cl MintRendering\Source\_UnityBuild.cpp /c /Fo_intermediate\MintRendering\ /Fd_o
 cl MintAudio\Source\_UnityBuild.cpp /c /Fo_intermediate\MintAudio\ /Fd_output\MintAudio.pdb
 cl MintPhysics\Source\_UnityBuild.cpp /c /Fo_intermediate\MintPhysics\ /Fd_output\MintPhysics.pdb
 cl MintGUI\Source\_UnityBuild.cpp /c /Fo_intermediate\MintGUI\ /Fd_output\MintGUI.pdb
+cl MintApp\Source\_UnityBuild.cpp /c /Fo_intermediate\MintApp\ /Fd_output\MintApp.pdb
 cl MintGame\Source\_UnityBuild.cpp /c /Fo_intermediate\MintGame\ /Fd_output\MintGame.pdb
 
 
 :### LINK
-lib _intermediate\MintCommon\Logger.obj /OUT:_output\MintCommon.lib
-lib _intermediate\MintContainer\UniqueString.obj /OUT:_output\MintContainer.lib
+lib _intermediate\MintCommon\CommonDefinitions.obj /OUT:_output\MintCommon.lib
+lib _intermediate\MintContainer\_UnityBuild.obj /OUT:_output\MintContainer.lib
 lib _intermediate\MintMath\_UnityBuild.obj /OUT:_output\MintMath.lib
 lib _intermediate\MintPlatform\_UnityBuild.obj /OUT:_output\MintPlatform.lib
 lib _intermediate\MintReflection\_UnityBuild.obj /OUT:_output\MintReflection.lib
@@ -68,12 +70,13 @@ lib _intermediate\MintRendering\_UnityBuild.obj /OUT:_output\MintRendering.lib
 lib _intermediate\MintAudio\_UnityBuild.obj /OUT:_output\MintAudio.lib
 lib _intermediate\MintPhysics\_UnityBuild.obj /OUT:_output\MintPhysics.lib
 lib _intermediate\MintGUI\_UnityBuild.obj /OUT:_output\MintGUI.lib
+lib _intermediate\MintApp\_UnityBuild.obj /OUT:_output\MintApp.lib
 lib _intermediate\MintGame\_UnityBuild.obj /OUT:_output\MintGame.lib
 
 
 :### COMPILE AND LINK <MintLibrary>
 cl MintLibrary\Source\_UnityBuild.cpp -c /Fo_intermediate\MintLibrary\ /Fd_output
-lib _intermediate\MintLibrary\_UnityBuild.obj /OUT:_output\MintLibrary.lib _output\MintCommon.lib _output\MintContainer.lib _output\MintMath.lib _output\MintPlatform.lib _output\MintReflection.lib _output\MintLanguage.lib _output\MintRenderingBase.lib _output\MintRendering.lib _output\MintAudio.lib _output\MintPhysics.lib _output\MintGUI.lib _output\MintGame.lib
+lib _intermediate\MintLibrary\_UnityBuild.obj /OUT:_output\MintLibrary.lib _output\MintCommon.lib _output\MintContainer.lib _output\MintMath.lib _output\MintPlatform.lib _output\MintReflection.lib _output\MintLanguage.lib _output\MintRenderingBase.lib _output\MintRendering.lib _output\MintAudio.lib _output\MintPhysics.lib _output\MintGUI.lib _output\MintApp.lib _output\MintGame.lib
 
 
 :### COMPILE THE FINAL EXECUTABLE!
