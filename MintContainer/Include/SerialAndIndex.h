5937bc242048f657a952bc73b5224dc85d68f499
principal6 <jesuskim666@gmail.com>
2025-05-22 00:43:18 +0900

    Update SerialAndIndex.h

--- MintContainer/Include/SerialAndIndex.h
+++ MintContainer/Include/SerialAndIndex.h
@@ -11,9 +11,13 @@
 
 namespace mint
 {
-	template<typename T, uint8 IndexBits>
-	class SerialAndIndex final
+	template<typename UintT, uint8 IndexBits>
+	class SerialAndIndex
 	{
+	public:
+		using UintType = UintT;
+		static constexpr uint8 kIndexBits = IndexBits;
+
 	public:
 		SerialAndIndex() : _raw{ 0 } { SetInvalidIndex(); }
 		SerialAndIndex(const SerialAndIndex& rhs) = default;
@@ -30,20 +34,20 @@ namespace mint
 
 	public:
 		MINT_INLINE void SetInvalidIndex() { SetSerialAndIndex(GetSerial(), GetInvalidIndex()); }
-		MINT_INLINE void SetSerialAndIndex(const T serial, const T index) { _raw = (serial << IndexBits) | index; }
+		MINT_INLINE void SetSerialAndIndex(const UintT serial, const UintT index) { _raw = (serial << IndexBits) | index; }
 
 	public:
-		MINT_INLINE constexpr T GetInvalidIndex() const { return (static_cast<T>(1) << IndexBits) - 1; }
+		MINT_INLINE constexpr UintT GetInvalidIndex() const { return (static_cast<UintT>(1) << IndexBits) - 1; }
 		MINT_INLINE bool IsValid() const { return HasValidSerial() == true && HasValidIndex() == true; }
 		MINT_INLINE bool HasValidIndex() const { return GetIndex() != GetInvalidIndex(); }
 		MINT_INLINE bool HasValidSerial() const { return GetSerial() != 0; }
-		MINT_INLINE T GetIndex() const { return _raw & IndexBits; }
-		MINT_INLINE T GetSerial() const { return _raw >> IndexBits; }
-		MINT_INLINE T GetRawValue() const { return _raw; }
+		MINT_INLINE UintT GetIndex() const { return _raw & IndexBits; }
+		MINT_INLINE UintT GetSerial() const { return _raw >> IndexBits; }
+		MINT_INLINE UintT GetRawValue() const { return _raw; }
 
 	protected:
-		static_assert(IndexBits < sizeof(T) * 8, "SerialAndIndex: No room for serial bits!");
-		std::enable_if_t<std::is_unsigned_v<T>, T> _raw;
+		static_assert(IndexBits < sizeof(UintT) * 8, "SerialAndIndex: No room for serial bits!");
+		std::enable_if_t<std::is_unsigned_v<UintT>, UintT> _raw;
 	};
 }
 
