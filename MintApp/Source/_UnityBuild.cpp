8cb1f2c68b25ee551d2adcc936bb4718845e494d
principal6 <jesuskim666@gmail.com>
2026-01-10 21:10:36 +0900

    Add SceneObject.cpp to unity build and exclude from build
    -

--- MintApp/Source/_UnityBuild.cpp
+++ MintApp/Source/_UnityBuild.cpp
@@ -1,4 +1,5 @@
 #include <MintApp/Source/DeltaTimer.cpp>
+#include <MintApp/Source/SceneObject.cpp>
 #include <MintApp/Source/SceneObjectRenderer.cpp>
 #include <MintApp/Source/SceneObjectSystems.cpp>
 #include <MintApp/Source/App.cpp>
